ff3f24d5b1b593ecf53a1ece04848855dc6d4680
manuo1 <manuo1@free.fr>
2025-11-20 13:38:56 +0100

    feat(notes): add Note model and admin

--- notes/admin.py
+++ notes/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from notes.models import Note
+
+
+@admin.register(Note)
+class NoteAdmin(admin.ModelAdmin):
+    list_display = ("id", "text_preview", "created_at", "updated_at")
+    list_filter = ("created_at", "updated_at")
+    search_fields = ("text",)
+
+    def text_preview(self, obj):
+        return obj.text[:50] + ("..." if len(obj.text) > 50 else "")
+
+    text_preview.short_description = "Text"
